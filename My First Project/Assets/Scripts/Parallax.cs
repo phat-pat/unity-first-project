f8068c1602f3af0aed70f373ca31d8013c343e08
Patrick Day <peday@ucdavis.edu>
2020-12-28 18:05:02 -0800

    Wrote parallax script (in new Scripts folder) and attached it to the hill scene.

---
+++ My First Project/Assets/Scripts/Parallax.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+
+    private float length, start, pos;
+    public float speed;
+
+    void Start() {
+        start = transform.position.x;
+        pos = start;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    void Update() {
+        pos += (speed * Time.deltaTime);
+        transform.position = new Vector3(pos, transform.position.y, transform.position.z);
+        
+        if (pos > start + length/2) {
+            pos -= length;
+        } else if (pos < start - length/2) {
+            pos += length;
+        }
+    }
+}
