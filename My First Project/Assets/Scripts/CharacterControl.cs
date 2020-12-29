d9d328a230bf43796d0f3d7d4599206cd1b17d56
Benjamin Coffman <bacoffman99@gmail.com>
2020-12-28 18:12:02 -0800

    Added script for animations with key presses (z,x,c)

---
+++ My First Project/Assets/Scripts/CharacterControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterControl : MonoBehaviour
+{
+
+    public GameObject thePlayer;
+    Animator animator;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            thePlayer.GetComponent<Animator>().Play("Charge_Attack_alt");
+        }
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            thePlayer.GetComponent<Animator>().Play("Blast_Attack_alt");
+        }
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            thePlayer.GetComponent<Animator>().Play("Death_alt");
+        }
+
+    }
+}
