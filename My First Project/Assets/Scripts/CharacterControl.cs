cf6f8377dbdf072a0b375dbc736086fd79f397df
Patrick Day <peday@ucdavis.edu>
2020-12-29 18:04:40 -0800

    Wrote most game logic, made shielding animation

--- My First Project/Assets/Scripts/CharacterControl.cs
+++ My First Project/Assets/Scripts/CharacterControl.cs
@@ -5,23 +5,61 @@ using UnityEngine;
 public class CharacterControl : MonoBehaviour
 {
 
-    public GameObject thePlayer;
-    Animator animator;
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Z))
-        {
-            thePlayer.GetComponent<Animator>().Play("Charge Attack");
-        }
-        if (Input.GetKeyDown(KeyCode.X))
-        {
-            thePlayer.GetComponent<Animator>().Play("Blast Attack");
-        }
-        if (Input.GetKeyDown(KeyCode.C))
-        {
-            thePlayer.GetComponent<Animator>().Play("Death");
-        }
+    public GameObject p1, p2;
+    public float countdown;
+    private string p1choice, p2choice;
+    private int p1power = 0, p2power = 0;
+    private bool gameOver;
+    private float startTime;
+    private Animator p1anim, p2anim;
 
+    void Start() {
+        startTime = Time.time;
+        p1anim = p1.GetComponent<Animator>();
+        p2anim = p2.GetComponent<Animator>();
+    }
+    void Update() {
+        if (!gameOver) {
+            if (Time.time - startTime < countdown) {
+                if (Input.GetKeyDown(KeyCode.Z)) p1choice = "Blast";
+                if (Input.GetKeyDown(KeyCode.X)) p1choice = "Charge";
+                if (Input.GetKeyDown(KeyCode.C)) p1choice = "Shield";
+                if (Input.GetKeyDown(KeyCode.LeftArrow)) p2choice = "Blast";
+                if (Input.GetKeyDown(KeyCode.DownArrow)) p2choice = "Charge";
+                if (Input.GetKeyDown(KeyCode.RightArrow)) p2choice = "Shield";
+            } else {
+                p1anim.SetTrigger(p1choice);
+                p2anim.SetTrigger(p2choice);
+                
+                switch (p1choice) {
+                    case "Charge":
+                        p1power++;
+                        if (p2choice == "Blast") { 
+                            p1anim.SetBool("Dead", true);
+                        }
+                        break;
+                    case "Blast":
+                        if (p2choice == "Charge") {
+                            p2anim.SetBool("Dead", true);
+                        }
+                        if (p2choice == "Charge") {
+                            if (p1power > p2power) {
+                                p2anim.SetBool("Dead", true);
+                            } 
+                            if (p1power < p2power) {
+                                p1anim.SetBool("Dead", true);
+                            }
+                        }
+                        p1power = 0;
+                        break;
+                    default:
+                        break;
+                }
+                if (p2choice == "Charge") p2power++;
+                if (p2choice == "Blast") p2power = 0;
+                if (p1anim.GetBool("Dead") || p2anim.GetBool("Dead")) gameOver = true;
+                if (!gameOver) startTime = Time.time;
+            }
+        }
     }
 }
---
+++ My First Project/Assets/TriggerHit.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerHit : MonoBehaviour
+{
+    public Animator OtherPlayerAnim;
+
+    public void Hit() {
+        if (OtherPlayerAnim.GetBool("Dead")) OtherPlayerAnim.Play("Hit");
+    }
+}
